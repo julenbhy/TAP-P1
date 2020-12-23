7ff6d7a89f5589dc0f496a282ed0831352826b2d
Julen Bohoyo <julen.bohoyo@estudiants.urv.cat>
2020-12-23 10:41:07 +0100

    decorator

---
+++ TAP-P1/src/PART2/WrapperForTheMailStore.java
@@ -0,0 +1,30 @@
+package PART2;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.List;
+ 
+import PART1.MailStore;
+import PART1.Message;
+
+//en el enunciado dice tenemos que usar Decorator y Strategy, osea que queremos hacer un decorator que a su vez dentro tenga
+//dos implementaciones gracias a strategi, strategi se suele hacer con interface, pero como ahora lo harermos dentro de un 
+//decorator pues tenemos que usar una abstract class.
+//en este enlace explican justo esto https://stackoverflow.com/questions/25098726/using-an-abstract-class-instead-of-the-interface-in-the-strategy-design-pattern
+public abstract class WrapperForTheMailStore implements MailStore{
+	
+	private MailStore client;
+	
+	public WrapperForTheMailStore(MailStore client) {
+		super();
+		this.client = client;
+	}
+
+	public void sendMail(Message mail) throws IOException
+	{
+		Message result = new Message(mail.getSender(), mail.getReceiver(), mail.getSubject(), mail.getBody().reverse());
+		client.sendMail(result);
+	}
+
+	public abstract List<Message> getMails(String user) throws FileNotFoundException;
+}
