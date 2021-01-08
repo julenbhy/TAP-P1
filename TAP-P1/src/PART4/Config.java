f87309365c233784e7c1c1530f7e71301298b101
Alberto Iglesias <alberto.iglesias@estudiants.urv.cat>
2021-01-08 20:21:56 +0100

    faltaba one thing xdd

--- TAP-P1/src/PART4/Config.java
+++ TAP-P1/src/PART4/Config.java
@@ -15,9 +15,16 @@ import java.lang.annotation.Target;
 @Target(TYPE)
 public @interface Config {
 	
-	
+	/**
+	 * Keep the info of which of the 3  MailStore we want to initialize
+	 * @return type of MailStore will be used
+	 */
 	String store() default "PART1.MailStoreMem";
 	
+	/**
+	 * Activate or disactivate log mode that prints on screen the whole methods done by the MailStore
+	 * @return boolean that indicates the activation
+	 */
 	boolean log() default true;
 
 }
